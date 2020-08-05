878c855b6dfefc1b671609744c097030d1be0620
Panos Athanasiou <panos_ath@outlook.com>
2020-08-05 14:46:15 +0300

    Add Helpers.ReactiveUI

---
+++ src/Helpers.ReactiveUI/Android/Extensions/FragmentExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Android.Views;
+using AndroidX.Fragment.App;
+using ReactiveUI.AndroidX;
+using static ReactiveUI.ControlFetcherMixin;
+
+namespace Panoukos41.Helpers
+{
+    public static class FragmentExtensions
+    {
+        public static View InflateAndWireUpControls(
+            this Fragment fragment,
+            LayoutInflater inflater,
+            ViewGroup viewGroup,
+            int layout,
+            ResolveStrategy resolveMembers = ResolveStrategy.Implicit,
+            bool attachToRoot = false)
+        {
+            var view = inflater.Inflate(layout, viewGroup, attachToRoot);
+            ControlFetcherMixin.WireUpControls(fragment, view, resolveMembers);
+            return view;
+        }
+    }
+}
